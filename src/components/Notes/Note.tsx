78c61704decf291b48c7593734800e1e9a2039ed
Tanya Saxena <82470912+saxenatanya@users.noreply.github.com>
2024-07-05 19:45:52 +0530

    bg color fix

--- src/components/Notes/Note.tsx
+++ src/components/Notes/Note.tsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
 import {notesType} from "../../type/KeepTypes";
 
 
 
 export const Note = (props:notesType)=> {
+    const [noteBgColor] = useState(props.bgColor);
+
     function handleClick() {
         props.onDelete(props.id);
     }
 
     return (
-        <div className="note" style={{backgroundColor:`${props.bgColor}`}}>
+        <div className="note" style={{backgroundColor:`${noteBgColor}`}}>
             <h1>{props.title}</h1>
             <p>{props.content}</p>
             <button onClick={handleClick}>
